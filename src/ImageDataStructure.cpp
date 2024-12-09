a27bee3d40081115673f46b3d22683c2ba6825bc
Yair-Steinlauf <yair.steinlauf@gmail.com>
2024-12-09 20:59:35 +0200

    .

--- src/ImageDataStructure.cpp
+++ src/ImageDataStructure.cpp
@@ -1,7 +1,7 @@
 #include "ImageDataStructure.h"
 
 ImageDataStructure::ImageDataStructure() :
-	m_Height(0), m_Width(0), m_image(nullptr);
+	m_Height(0), m_Width(0), m_image(nullptr){}
 
 ImageDataStructure::ImageDataStructure(int height, int width)
 	:ImageDataStructure(height, width, Pixel())
