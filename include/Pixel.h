056ff7dea48ce064ac94183cc9662319e8435da1
Yair-Steinlauf <yair.steinlauf@gmail.com>
2024-12-13 02:29:51 +0200

    Pixel oparator global and works!

--- include/Pixel.h
+++ include/Pixel.h
@@ -7,9 +7,16 @@ const unsigned char WHITE = ' ';
 
 class Pixel {
 public:
-	Pixel(unsigned char pixel = ' ');
-
+	Pixel(unsigned char color = ' ');
+	unsigned char getColor()const;
 private:
 	unsigned char m_color = WHITE;
 
 };
+bool operator==(const Pixel& a, const Pixel& b);
+bool operator!=(const Pixel& a, const Pixel& b);
+Pixel operator&(const Pixel& a, const Pixel& b);
+Pixel& operator&=(Pixel& a, const Pixel& b);
+Pixel operator|(const Pixel& a, const Pixel& b);
+Pixel& operator|=(Pixel& a, const Pixel& b);
+std::ostream& operator<<(std::ostream& os, const Pixel& other);
--- src/Pixel.cpp
+++ src/Pixel.cpp
@@ -1,2 +1,56 @@
 #include "Pixel.h"
 #include <ostream>
+
+Pixel::Pixel(unsigned char color)
+	:m_color(color){}
+
+
+unsigned char Pixel::getColor() const
+{
+	return m_color;
+}
+
+
+bool operator==(const Pixel& a, const Pixel& b)
+{
+	return a.getColor() == b.getColor();
+}
+
+bool operator!=(const Pixel& a, const Pixel& b)
+{
+	return a.getColor() != b.getColor();
+}
+
+Pixel operator&(const Pixel& a, const Pixel& b)
+{
+	if (a.getColor()< b.getColor())
+		return Pixel(a.getColor());
+	return Pixel(b.getColor());
+}
+
+Pixel& operator&=(Pixel& a, const Pixel& b)
+{
+	auto temp = a & b;
+	a = temp;
+	return temp;
+}
+
+Pixel operator|(const Pixel& a, const Pixel& b)
+{
+	if (a.getColor() > b.getColor())
+		return Pixel(a.getColor());
+	return Pixel(b.getColor());
+}
+
+Pixel& operator|=(Pixel& a, const Pixel& b)
+{
+	auto temp = a | b;
+	a = temp;
+	return temp;
+}
+
+std::ostream& operator<<(std::ostream& os, const Pixel& pixel)
+{
+	os << pixel.getColor();
+	return os;
+}
