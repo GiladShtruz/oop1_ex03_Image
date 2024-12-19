187b50660043c89a72c251278b2461ade0774e80
Yair-Steinlauf <yair.steinlauf@gmail.com>
2024-12-19 11:20:55 +0200

    ready to SUBMIT

--- include/ImageDataStructure.h
+++ include/ImageDataStructure.h
@@ -17,34 +17,3 @@ private:
 	int mHeight;
 	int mWidth;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-//class ImageDataStructure {
-//
-//public:
-//	ImageDataStructure();
-//	ImageDataStructure(int height, int width);
-//	ImageDataStructure(int height, int width, Pixel pixel);
-//	Pixel getPixel()const;
-//	void setPixel();
-//	~ImageDataStructure();
-//
-//private:
-//	Pixel** cloneImage(const ImageDataStructure& other);
-//	Pixel** whiteImage(int height, int width);
-//	Pixel** colorImage(int height, int width, Pixel pixel);
-//	Pixel** mImage;
-//	unsigned int mHeight;
-//	unsigned int mWidth;
-//};
